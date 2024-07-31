a5c030b6591ba42353de46fb9d8dc575fd64ffcc
ColaChoco <walkeralan620@gmail.com>
2024-08-01 00:37:26 +0700

    This is done US06 and T01 of US01

---
+++ UserStory 01,05,06/AgileEcommerce/AgileEcommerce/Data/Role.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AgileEcommerce.Data;
+
+public partial class Role
+{
+    public string Id { get; set; }
+
+    public string NameRole { get; set; }
+
+    public string NormalizedName { get; set; }
+
+    public virtual ICollection<User> Users { get; set; } = new List<User>();
+}
