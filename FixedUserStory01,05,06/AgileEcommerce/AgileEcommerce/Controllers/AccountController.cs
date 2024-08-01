3c8408b50c55354f81d97bdc1de2428c1d6b19f1
aunguyen2511 <aunguyen2511@gmail.com>
2024-08-01 13:27:37 +0700

    Fixed lai cai login va register roi nha

---
+++ FixedUserStory01,05,06/AgileEcommerce/AgileEcommerce/Controllers/AccountController.cs
@@ -0,0 +1,67 @@
+﻿using AgileEcommerce.Data;
+using AgileEcommerce.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Identity.Client;
+
+namespace AgileEcommerce.Controllers
+{
+    public class AccountsController : Controller
+    {
+        private readonly AccountContext _context;
+        public AccountsController(AccountContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var data = _context.Users != null ? _context.Users.ToList() : new List<User>();
+            return View(data);
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register(RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new User
+                {
+                    Id = Guid.NewGuid().ToString(),
+                    UserName = model.UserName,
+                    FullName = model.FullName,
+                    Email = model.Email,
+                    PhoneNumber = model.PhoneNumber,
+                    PasswordHash = BCrypt.Net.BCrypt.HashPassword(model.Password)
+                };
+                _context.Users.Add(user);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(model);
+        }
+
+        [HttpGet]
+        public IActionResult Login() { return View(); }
+
+        [HttpPost]
+        public IActionResult Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = _context.Users.FirstOrDefault(p => p.UserName == model.UserName);
+                if (user != null && BCrypt.Net.BCrypt.Verify(model.Password, user.PasswordHash))
+                {
+                    return RedirectToAction("Index");
+                }
+                ModelState.AddModelError("", "Hãy thử đăng nhập lại.");
+            }
+            return View(model);
+        }
+    }
+}
