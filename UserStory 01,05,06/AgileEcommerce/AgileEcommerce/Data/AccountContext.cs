bf9b8f0936dd85a1e792f0e6f0abe7c0213dafe3
ColaChoco <walkeralan620@gmail.com>
2024-08-01 13:35:49 +0700

    Raw Code cho User Story 01, 05 va 06

---
+++ UserStory 01,05,06/AgileEcommerce/AgileEcommerce/Data/AccountContext.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace AgileEcommerce.Data;
+
+public partial class AccountContext : DbContext
+{
+    public AccountContext()
+    {
+    }
+
+    public AccountContext(DbContextOptions<AccountContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Role> Roles { get; set; }
+
+    public virtual DbSet<User> Users { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlServer("Server=DESKTOP-14SO8E0\\SQLEXPRESS; Database=ACCOUNT;Integrated Security=True; Trust Server Certificate=True;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Role>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Roles__3214EC07DD572029");
+
+            entity.Property(e => e.NameRole).HasMaxLength(256);
+            entity.Property(e => e.NormalizedName).HasMaxLength(256);
+        });
+
+        modelBuilder.Entity<User>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Users__3214EC07B3597EB6");
+
+            entity.Property(e => e.Email).HasMaxLength(256);
+            entity.Property(e => e.FullName).HasMaxLength(256);
+            entity.Property(e => e.UserName).HasMaxLength(256);
+
+            entity.HasMany(d => d.Roles).WithMany(p => p.Users)
+                .UsingEntity<Dictionary<string, object>>(
+                    "UserRole",
+                    r => r.HasOne<Role>().WithMany()
+                        .HasForeignKey("RoleId")
+                        .HasConstraintName("FK__UserRoles__RoleI__3C69FB99"),
+                    l => l.HasOne<User>().WithMany()
+                        .HasForeignKey("UserId")
+                        .HasConstraintName("FK__UserRoles__UserI__3B75D760"),
+                    j =>
+                    {
+                        j.HasKey("UserId", "RoleId").HasName("PK__UserRole__AF2760AD8192D65A");
+                        j.ToTable("UserRoles");
+                    });
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ UserStory 01,05,06/AgileEcommerce/AgileEcommerce/Data/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AgileEcommerce.Data;
+
+public partial class User
+{
+    public string Id { get; set; }
+
+    public string UserName { get; set; }
+
+    public string FullName { get; set; }
+
+    public string Email { get; set; }
+
+    public string PasswordHash { get; set; }
+
+    public string PhoneNumber { get; set; }
+
+    public virtual ICollection<Role> Roles { get; set; } = new List<Role>();
+
+}
