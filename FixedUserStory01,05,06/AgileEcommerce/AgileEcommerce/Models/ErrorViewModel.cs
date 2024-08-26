5973f90c3f45c59e5cf2ed4d5bbfc92a9524291e
aunguyen2511 <aunguyen2511@gmail.com>
2024-08-27 00:57:25 +0700

    Day la chuc nang CRUD cho Loai/ Danh muc san pham - Category

--- FixedUserStory01,05,06/AgileEcommerce/AgileEcommerce/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace AgileEcommerce.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
