18a4ce78a9f3f6316d9b30dfe10866d66e7ed938
CocaChoco <walkeralan620@gmail.com>
2024-08-27 13:56:42 +0700

    Code bao gồm fix Delete Category, Trang tạo tài khoản dảnh cho Admin và quản lí tài khoản chưa CRUD

---
+++ AgileCommercee/AgileCommercee/Controllers/LoaiController.cs
@@ -0,0 +1,83 @@
+﻿using AgileCommercee.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+
+namespace AgileCommercee.Controllers
+{
+    public class LoaisController : Controller
+    {
+        private readonly AgileStoreContext _Context;
+        public LoaisController(AgileStoreContext context)
+        {
+            _Context = context;
+        }
+        public IActionResult Index()
+        {
+            if (!ModelState.IsValid)
+            {
+                ModelState.AddModelError("loi", "Còn lỗi");
+            }
+            var data = _Context.Loais != null ? _Context.Loais.ToList() : new List<Loai>();
+            return View(data);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Loai model, IFormFile FileLogo)
+        {
+            if (!ModelState.IsValid)
+            {
+                ModelState.AddModelError("loi", "Còn lỗi");
+            }
+            if (FileLogo != null)
+            {
+                model.Hinh = MyTool.UploadImageToFolder(FileLogo, "Hinh");
+            }
+            try
+            {
+                _Context.Add(model);
+                _Context.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {
+
+            }
+            return View();
+        }
+
+        // GET: Loai/Delete
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            var loai = _Context.Loais.SingleOrDefault(x => x.MaLoai == id);
+            if (loai != null)
+            {
+                return View(loai);
+            }
+            return NotFound();
+        }
+
+        // POST: Loai/ConfirmDelete
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult ConfirmDelete(int id)
+        {
+            var loai = _Context.Loais.SingleOrDefault(x => x.MaLoai == id);
+            if (loai != null)
+            {
+                _Context.Loais.Remove(loai);
+                _Context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return NotFound();
+        }
+    }
+}
