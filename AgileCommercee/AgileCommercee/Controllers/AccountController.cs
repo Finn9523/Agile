70356c208eb5faee59d9b7c25c644aabd6f03726
CocaChoco <walkeralan620@gmail.com>
2024-08-29 15:37:19 +0700

    Code trong đây bao gồm CRUD cho HangHoas và sửa layout cho vài Views, sửa cả hiển thị ảnh sản phẩm và hiện sản phẩm trên trang chủ

---
+++ AgileCommercee/AgileCommercee/Controllers/AccountController.cs
@@ -0,0 +1,181 @@
+﻿
+using AgileCommercee.Data;
+using AgileCommercee.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Microsoft.Identity.Client;
+
+namespace AgileCommercee.Controllers
+{
+    public class AccountsController : Controller
+    {
+        private readonly AgileStoreContext _context;
+        public AccountsController(AgileStoreContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var data = _context.Users != null ? _context.Users.ToList() : new List<User>();
+            return View(data);
+        }
+
+        public IActionResult Main()
+        {
+            var products = _context.HangHoas.ToList();
+            ViewBag.Products = products;
+            return View(products);
+        }
+        public IActionResult AfterLogin()
+        {
+            var products = _context.HangHoas.ToList();
+            return View(products);
+        }
+
+        public IActionResult CustomerLogin()
+        {
+            var products = _context.HangHoas.ToList();
+            return View(products);
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register(RegisterViewModel model, IFormFile Hinh)
+        {
+            if (Hinh != null)
+            {
+                model.Hinh = MyTool.UploadImageToFolder(Hinh, "Hinh");
+            }
+            try
+            {
+                    var user = new User
+                    {
+                        UserName = model.UserName,
+                        FullName = model.FullName,
+                        Email = model.Email,
+                        Address = model.Address,
+                        PhoneNumber = model.PhoneNumber,
+                        PasswordHash = BCrypt.Net.BCrypt.HashPassword(model.Password),
+                        Hinh = model.Hinh,
+                        Roles = 1
+                    };
+                    _context.Add(user);
+                    _context.SaveChanges();
+
+                    return RedirectToAction("Main");
+            }
+            catch (Exception ex) 
+            {
+                Console.WriteLine(ex.Message);
+            }
+            return RedirectToAction("Main");
+        }
+
+        [HttpGet]
+        public IActionResult RegisterAD() { return View(); }
+
+        [HttpPost]
+        public IActionResult RegisterAD(AdminRegisterViewModel model, IFormFile Hinh)
+        {
+            if (Hinh != null)
+            {
+                model.Hinh = MyTool.UploadImageToFolder(Hinh, "Hinh");
+            }
+            try
+            {
+                var user = new User
+                {
+                    UserName = model.UserName,
+                    FullName = model.FullName,
+                    Email = model.Email,
+                    Address = model.Address,
+                    PhoneNumber = model.PhoneNumber,
+                    PasswordHash = BCrypt.Net.BCrypt.HashPassword(model.Password),
+                    Hinh = model.Hinh,
+                    Roles = model.Roles  
+                };
+                _context.Add(user);
+                _context.SaveChanges();
+                return RedirectToAction("AfterLogin");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            return RedirectToAction("AfterLogin");
+        }
+
+        [HttpGet]
+        public IActionResult Login() { return View(); }
+
+        [HttpPost]
+        public IActionResult Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = _context.Users.FirstOrDefault(p => p.UserName == model.UserName);
+                if (user != null && BCrypt.Net.BCrypt.Verify(model.Password, user.PasswordHash))
+                {
+                    /*
+                    HttpContext.Session.SetString("UserName", user.FullName);
+                    HttpContext.Session.SetString("UserImage", user.Hinh);
+                    */
+                    if (user.Roles == 0)
+                    {
+                        return RedirectToAction("AfterLogin");
+                    }
+                    else if (user.Roles == null)
+                    {
+                        return RedirectToAction("CustomerLogin");
+                    }
+                }
+                ModelState.AddModelError("", "Hãy thử đăng nhập lại.");
+            }
+            return View(model);
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var existedUser = _context.Users.SingleOrDefault(x => x.Id == id);
+            if (existedUser != null)
+            {
+                return View(existedUser);
+            }
+            return NotFound();
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Account model, IFormFile FileLogo)
+        {
+            var existedUser = _context.Users.SingleOrDefault(x => x.Id == model.Id);
+            if(existedUser != null)
+            {
+                try
+                {
+                    existedUser.FullName = model.FullName;
+                    existedUser.UserName = model.UserName;
+                    existedUser.Address = model.Address;
+                    existedUser.PhoneNumber = model.PhoneNumber;
+                    if (FileLogo != null)
+                    {
+                        existedUser.Hinh = MyTool.UploadImageToFolder(FileLogo, "Hinh");
+                    }
+                    _context.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+                catch (Exception ex) 
+                {
+                    
+                }
+            }
+            return NotFound();
+        }
+    }
+}
---
+++ AgileCommercee/AgileCommercee/Controllers/HangHoaController.cs
@@ -0,0 +1,178 @@
+﻿using AgileCommercee.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace AgileCommercee.Controllers
+{
+    public class HangHoasController : Controller
+    {
+        public readonly AgileStoreContext _context;
+        public HangHoasController(AgileStoreContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            if (!ModelState.IsValid)
+            {
+                ModelState.AddModelError("loi", "Còn lỗi");
+            }
+            var data = _context.HangHoas != null ? _context.HangHoas.ToList() : new List<HangHoa>();
+            return View(data);
+        }
+
+        public IActionResult IndexCus()
+        {
+            if (!ModelState.IsValid)
+            {
+                ModelState.AddModelError("loi", "Còn lỗi");
+            }
+            var data = _context.HangHoas != null ? _context.HangHoas.ToList() : new List<HangHoa>();
+            return View(data);
+        }
+
+        public IActionResult Product()
+        {
+            return RedirectToAction("Index");
+        }
+        #region Create HangHoa
+        // GET: HangHoas/Create
+        [HttpGet]
+        public IActionResult Create()
+        {
+            ViewBag.Loais = new SelectList(_context.Loais.ToList(), "MaLoai", "TenLoai");
+            ViewBag.NhaCungCaps = new SelectList(_context.NhaCungCaps.ToList(), "MaNcc", "TenCongTy");
+            return View();
+        }
+        // POST: HangHoas/Create
+        [HttpPost]
+        public IActionResult Create(HangHoa model, IFormFile Hinh)
+        {
+            if (!ModelState.IsValid)
+            {
+                ModelState.AddModelError("loi", "Còn lỗi");
+            }
+            ViewBag.Loais = new SelectList(_context.Loais.ToList(), "MaLoai", "TenLoai");
+            ViewBag.NhaCungCaps = new SelectList(_context.NhaCungCaps.ToList(), "MaNcc", "TenCongTy");
+            try
+            {
+                //upload field logo
+                model.Hinh = MyTool.UploadImageToFolder(Hinh, "Hinh");
+                _context.Add(model);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {
+            }
+            return View();
+        }
+        #endregion
+        public PartialViewResult Search(string s)
+        {
+            var results = _context.HangHoas
+                .Where(hh => hh.TenHh.Contains(s))
+                .ToList();
+
+            return PartialView("_SearchResults", results);
+        }
+
+        [HttpGet]
+        public IActionResult Details() { return View(); }
+
+        [HttpPost]
+        public IActionResult Details(int Id, IFormFile Hinh)
+        {
+            var product = _context.HangHoas.SingleOrDefault(x => x.MaHh == Id);
+            if (product != null)
+            {
+                return View(product);
+            }
+            return NotFound();
+        }
+
+        [HttpGet]
+        public IActionResult DetailsCus() { return View(); }
+        [HttpPost]
+        public IActionResult DetailsCus(int Id, IFormFile Hinh)
+        {
+            var product = _context.HangHoas.SingleOrDefault(x => x.MaHh == Id);
+            if (product != null)
+            {
+                return View(product);
+            }
+            return NotFound();
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int Id)
+        {
+            var existedProduct = _context.HangHoas.SingleOrDefault(x => x.MaHh == Id);
+            ViewBag.Loais = new SelectList(_context.Loais.ToList(), "MaLoai", "TenLoai");
+            ViewBag.NhaCungCaps = new SelectList(_context.NhaCungCaps.ToList(), "MaNcc", "TenCongTy");
+            if (existedProduct != null)
+            {
+                return View(existedProduct);
+            }
+            return NotFound();
+        }
+        [HttpPost]
+        public IActionResult Edit(HangHoa model, IFormFile Hinh)
+        {
+            var existedProduct = _context.HangHoas.SingleOrDefault(x => x.MaHh == model.MaHh);
+            ViewBag.Loais = new SelectList(_context.Loais.ToList(), "MaLoai", "TenLoai");
+            ViewBag.NhaCungCaps = new SelectList(_context.NhaCungCaps.ToList(), "MaNcc", "TenCongTy");
+            try
+            {
+                existedProduct.TenHh = model.TenHh;
+                ViewBag.existedProduct.MaLoai = model.MaLoai;
+                existedProduct.MoTaDonVi = model.MoTaDonVi;
+                existedProduct.DonGia = model.DonGia;
+                existedProduct.NgaySx = model.NgaySx;
+                existedProduct.GiamGia = model.GiamGia;
+                existedProduct.MoTa = model.MoTa;
+                ViewBag.existedProduct.MaNcc = model.MaNcc;
+                if (Hinh == null)
+                {
+                    existedProduct.Hinh = model.Hinh;
+                }
+                else
+                {
+                    existedProduct.Hinh = MyTool.UploadImageToFolder(Hinh, "Hinh");
+                }
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int Id)
+        { 
+            var product = _context.HangHoas.SingleOrDefault(x => x.MaHh == Id);
+            if(product != null)
+            {
+                return View(product);
+            }
+            return NotFound();
+        }
+        
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult ConfirmDelete(int Id) 
+        {
+            var product = _context.HangHoas.SingleOrDefault(x => x.MaHh == Id);
+            if(product != null)
+            {
+                _context.HangHoas.Remove(product);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return NotFound();
+        }
+    }
+}
+
